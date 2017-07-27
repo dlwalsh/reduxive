2e3c0ef941d8124429bf9f4bd738955fda02eadb
David Walsh <dlwalsh@gmail.com>
2017-07-27 16:28:42 +1000

    add action param to update reducer

--- src/update.js
+++ src/update.js
@@ -3,13 +3,13 @@ import { mapValues, isPlainObject } from 'lodash/fp';
 const mapValuesWithKey = mapValues.convert({ cap: false });
 
 function update(delta = {}) {
-  return (state) => {
+  return (state, action) => {
     const newState = mapValuesWithKey((value, key) => {
       if (isPlainObject(value)) {
-        return update(value)(state[key]);
+        return update(value)(state[key], action);
       }
       if (typeof value === 'function') {
-        return value(state[key]);
+        return value(state[key], action);
       }
       return value;
     })(delta);
---
+++ tests/update.test.js
@@ -0,0 +1,64 @@
+/* eslint import/no-extraneous-dependencies: "off" */
+
+import test from 'tape';
+import update from '../src/update.js';
+
+test('simple update', ({ deepEqual, end }) => {
+  const reducer = update({
+    a: 2,
+  });
+
+  deepEqual(reducer({ a: 1 }), { a: 2 }, 'state overwritten correctly');
+  deepEqual(reducer({ a: 1, b: 1 }), { a: 2, b: 1 }, 'state updated correctly');
+  end();
+});
+
+test('simple update', ({ deepEqual, end }) => {
+  const reducer = update({
+    a: x => x + 1,
+  });
+  const result = reducer({ a: 1 });
+
+  deepEqual(result, { a: 2 }, 'state updated correctly');
+  end();
+});
+
+test('nested update', ({ deepEqual, end }) => {
+  const reducer = update({
+    a: {
+      b: x => x + 1,
+    },
+  });
+  const result = reducer({ a: { b: 1 } });
+
+  deepEqual(result, { a: { b: 2 } }, 'deep state updated correctly');
+  end();
+});
+
+test('update from action', ({ deepEqual, end }) => {
+  const reducer = update({
+    a: (x, action) => action.value,
+  });
+  const result = reducer({ a: 0 }, { value: 100 });
+
+  deepEqual(result, { a: 100 }, 'state updated from action');
+  end();
+});
+
+test('update nested object from action', ({ deepEqual, end }) => {
+  const reducer = update({
+    a: (x, action) => x + action.value,
+    b: {
+      c: (x, action) => action.value,
+    },
+  });
+  const result = reducer({
+    a: 50,
+    b: {
+      c: 10,
+    },
+  }, { value: 100 });
+
+  deepEqual(result, { a: 150, b: { c: 100 } }, 'nested state updated from action');
+  end();
+});
